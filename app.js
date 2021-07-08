c47360cba85b0136559496efaaf7a7e5874563d6
arthurbbarros <arthurbaraujo92@gmail.com>
2021-07-08 17:02:31 -0300

    teste final discord bot

--- app.js
+++ app.js
@@ -2,29 +2,31 @@
 
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+const films = require('./films.js')
 
 bot.on('message', receiveMessage)
 
 function receiveMessage(mensagem){
-    
-    const films = [{nome:"Uma Nova Esperança", ano:1977, ordenacao:1, secao:"Trilogia Clássica", imagem:"https://bit.ly/2TIetq4"},{nome:"O Império Contra Ataca",ano:1980,ordenacao:2,secao:"Trilogia Clássica",imagem:"https://bit.ly/2TIetq4"},{nome:"O retorno Jedi",ano:1982,ordenacao:3,secao:"Trilogia Clássica",imagem:"https://bit.ly/3cNern8"},{nome:"A Ameaça Fantasma",ano:1993,ordenacao:4,secao:"Trilogia Prequel",imagem:"https://bit.ly/3xsioFX"},{nome:"Ataque dos Clones",ano:2002,ordenacao:5,secao:"Trilogia Prequel",imagem:"https://bit.ly/3vur0ud"},{nome:"A Vingança dos Sith",ano:2005,ordenacao:6,secao:"Trilogia Prequel",imagem:"https://bit.ly/35qWXZJ"},{nome:"Rogue One",ano:2016,ordenacao:7,secao:"Spin- Off",imagem:"https://bit.ly/3guHP44"},{nome:"Han Solo",ano:2018,ordenacao:8,secao:"Spin- Off",imagem:"https://bit.ly/3iOIa34"},{nome:"O Despertar da Força",ano:2015,ordenacao:9,secao:"Trilogia nova",imagem:"https://bit.ly/3zsIzOJ"},{nome:"Os Últimos Jedi",ano:2017,ordenacao:10,secao:"Trilogia nova",imagem:"https://bit.ly/3xvaPhw"},{nome:"A Ascenção de Skywalker",ano:2019,ordenacao:11,secao:"Trilogia nova",imagem:"https://bit.ly/3vx59lS"}] 
-
-    
     if(mensagem.author.bot == false){
-        if(mensagem.content == "01"){
-           films.map((film) => {
+        if(mensagem.content == 'Qual melhor forma de entender a franquia de Star Wars?'){
+           films.map((movie) => {
         
                 const msg = new Discord.MessageEmbed()
-                msg.setTitle(film.nome)
-                msg.setDescription(film.secao)
+                msg.setTitle(movie.nome)
+                msg.setFooter(movie.secao)
                 msg.setColor("#bb22e6")
-                msg.setThumbnail(film.imagem)
+                msg.setThumbnail("https://i.pinimg.com/originals/4f/71/49/4f7149757a746c4464c97314d1a41cd1.png")
+                msg.setImage(movie.imagem)
+                msg.setDescription(movie.ano)
+                    
+                
                 
                 mensagem.channel.send(msg)
             })
         } else mensagem.channel.send(`Esta é uma mensagem automática`)
     }
 }
+
 
 
 bot.login('ODYyMTEzMDk3Njk4NTA4ODAx.YOTnYw.oeGGHLZC_lqFVLcwQK1G6UNlyro')
---
+++ films.js
@@ -0,0 +1,15 @@
+const films = [
+{nome:"Uma Nova Esperança", ano:1977, ordenacao:1, secao:"Trilogia Clássica", imagem:"https://bit.ly/2TIetq4"},
+{nome:"O Império Contra Ataca",ano:1980,ordenacao:2,secao:"Trilogia Clássica",imagem:"https://bit.ly/2TIetq4"},
+{nome:"O retorno Jedi",ano:1982,ordenacao:3,secao:"Trilogia Clássica",imagem:"https://bit.ly/3cNern8"},
+{nome:"A Ameaça Fantasma",ano:1993,ordenacao:4,secao:"Trilogia Prequel",imagem:"https://bit.ly/3xsioFX"},
+{nome:"Ataque dos Clones",ano:2002,ordenacao:5,secao:"Trilogia Prequel",imagem:"https://bit.ly/3vur0ud"},
+{nome:"A Vingança dos Sith",ano:2005,ordenacao:6,secao:"Trilogia Prequel",imagem:"https://bit.ly/35qWXZJ"},
+{nome:"Rogue One",ano:2016,ordenacao:7,secao:"Spin- Off",imagem:"https://bit.ly/3guHP44"},
+{nome:"Han Solo",ano:2018,ordenacao:8,secao:"Spin- Off",imagem:"https://bit.ly/3iOIa34"},
+{nome:"O Despertar da Força",ano:2015,ordenacao:9,secao:"Trilogia nova",imagem:"https://bit.ly/3zsIzOJ"},
+{nome:"Os Últimos Jedi",ano:2017,ordenacao:10,secao:"Trilogia nova",imagem:"https://bit.ly/3xvaPhw"},
+{nome:"A Ascenção de Skywalker",ano:2019,ordenacao:11,secao:"Trilogia nova",imagem:"https://bit.ly/3vx59lS"}
+]
+
+module.exports = films
